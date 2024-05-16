c5a6b6d9776bd1618453937bd52542e90f1b76ea
faisal <mohd.faisal@blubook.co.in>
2024-05-16 15:39:14 +0530

    changing-product operator to addition

--- operatorOverloading2.cpp
+++ operatorOverloading2.cpp
@@ -1,5 +1,31 @@
 #include<iostream>
 using namespace std;
+
+class A{
+    int a;
+
+    public:
+    void getdata(){
+        cout<<"Enter the value :\n";
+        cin>>a;
+    }
+    void showdata(){
+        cout<<"you value is :\t"<<a;
+    }
+    A operator *(A aa){
+        A x;
+        x.a=a+aa.a;
+        return x;
+
+    }
+};
 int main(){
+    A x,y,z;
+    x.getdata();
+    y.getdata();
+    z=x*y;
+cout<<"on multiplying you will get the result of addition\n";
+    z.showdata();
     
+
 }
